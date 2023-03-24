7ffdf3d6008213cd26e6bef282c674229cc154af
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-03-24 11:45:34 +0200

    Create Navigation.js

---
+++ client/src/pages/navigation/Navigation.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from '@/styles/Home.module.css'
+
+export default function Navigation() {
+    return (
+        <ul className={styles.grid}>
+            <li className={styles.nav}>
+              <Link href="/api/hello">Go to</Link>
+            </li>
+            <li className={styles.nav}>
+              <Link href="/api/hello">Go to</Link>
+            </li>
+            <li className={styles.nav}>
+              <Link href="/api/hello">Go to</Link>
+            </li>
+          </ul>
+    )
+}
