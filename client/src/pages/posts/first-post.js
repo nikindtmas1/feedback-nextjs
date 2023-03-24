1d984804f6685f6d8f3d627148de8e22c89312d2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-03-24 13:32:12 +0200

    Added element

--- client/src/pages/posts/first-post.js
+++ client/src/pages/posts/first-post.js
@@ -3,11 +3,13 @@ import styles from '@/styles/Home.module.css'
 
 export default function PostOne() {
     return (
-        <>
-        <h1>First Post</h1>
-        <a>
-            <Link href='/'>Back to home</Link>
-        </a>
-        </>
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <h1>First Post</h1>
+                <a>
+                    <Link href='/'>Back to home</Link>
+                </a>
+            </div>
+        </main>
     )
 }
