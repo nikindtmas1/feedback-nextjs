e0fa031318f3ca61daf1af8ead28648ba73fb071
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-03-24 12:00:11 +0200

    Change import

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Navigation from './navigation/navigation'
+import Navigation from '@/navigation/Navigation'
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
