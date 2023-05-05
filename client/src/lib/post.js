6e7c7a4b87804ded6deb45dea4bfa19ddaeba6c1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-05-05 13:19:24 +0300

    Convert responce to json

--- client/src/lib/post.js
+++ client/src/lib/post.js
@@ -3,7 +3,7 @@ export async function getSortedPostsData() {
     
     const responce = await fetch("http://localhost:5000/goshofeedbacks");
     const jsonData = await responce.json();
-    const result = Object.values(jsonData);
-    // console.log(result);
+    // const result = Object.values(jsonData);
+    // console.log(jsonData);
     return jsonData;
 };
