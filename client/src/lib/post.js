75c7ff065cb90447f67c8490c6bad3fcf81b6182
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-05-05 10:49:24 +0300

    Update post.js

--- client/src/lib/post.js
+++ client/src/lib/post.js
@@ -1,8 +1,8 @@
 
 export async function getSortedPostsData() {
     
-    const responce = await fetch("http://example.com/movies.json");
-    const jsonData = await responce.json();
-    console.log(jsonData);
-    //return res.json();
+    const responce = await fetch("...");
+    // const jsonData = await responce.json();
+    // console.log(jsonData);
+    // return responce;
 };
