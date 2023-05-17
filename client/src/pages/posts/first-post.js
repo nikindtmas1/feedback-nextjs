eafe5f9cb227e439a81b1b79d23a969985e7ea5d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-05-17 13:26:44 +0300

    Restore page

---
+++ client/src/pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Layout from "../../../components/layout";
+
+export default function PostOne() {
+    return (
+        <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href='/'>← Back to home</Link>
+                </h2>
+   
+        </Layout>
+    )
+}
