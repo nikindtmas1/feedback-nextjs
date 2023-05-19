9a6549f1f9143038547a8ead6824573ec1a2f51a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-05-19 10:45:56 +0300

    Update goshoData.js

--- client/src/pages/posts/goshoData.js
+++ client/src/pages/posts/goshoData.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout from "../../../components/layout";
 import Link from "next/link";
+import styles from '../../styles/Home.module.css';
 
 export async function getServerSideProps() {
   const goshoData = await fetch('http://localhost:5000/goshofeedbacks');
@@ -20,10 +21,41 @@ export default function GoshoPosts({allGoshoData}) {
         <>
             <Head>
                 <title>
-                    This is Gosho Data Page
+                    Gosho Data
                 </title>
             </Head>
+            <h2>
+            This is Gosho Data Page
+          </h2>
 
+            <main className={styles.main}>
+        <div className={styles.description}>
+          {allGoshoData.map(({text, rating,peopleName,userName}) => (
+           <div className={styles.card}>
+          <div className={''}>
+             {rating}
+          </div>
+              <div>
+                {text}
+              </div>
+           <div>
+             {peopleName}
+           </div>
+            <div>
+             {userName}
+            </div>
+           </div>
+           
+          ))}
+       
+        </div>
+
+      <Layout>
+                <h2>
+                    <Link href='/'>← Back to home</Link>
+                </h2>
+      </Layout>
+      </main>
         <Layout>
             <h2>
             <Link href='/'>← Back to home</Link>
