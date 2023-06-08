00e4472fd2e3a70f4dbe403959f54ccdb299cac0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-06-08 11:55:11 +0300

    Added tags

--- client/src/pages/posts/toshoData.js
+++ client/src/pages/posts/toshoData.js
@@ -1,4 +1,25 @@
+import Head from "next/head";
+import Layout from "../../../components/layout";
 
-export default async function ToshoData({}) {
-    
+export default async function ToshoPosts({}) {
+    return (
+        <>
+            <Head>
+                <title>
+                    Tosho Data
+                </title>
+            </Head>
+            <h2>
+                This Tosho Data Page
+            </h2>
+
+            <main>
+                <Layout>
+                <h2>
+                    <Link href='/'>← Back to home</Link>
+                </h2>
+                </Layout>
+            </main>
+        </>
+    )
 };
