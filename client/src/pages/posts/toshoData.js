d38592bca53b9c282d19412a9d506f334544cc5d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-06-08 12:47:55 +0300

    Added function getServerSideProps

--- client/src/pages/posts/toshoData.js
+++ client/src/pages/posts/toshoData.js
@@ -3,7 +3,22 @@ import Layout from "../../../components/layout";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
-export default function ToshoPosts({}) {
+export async function getServerSideProps() {
+    const toshoData = await fetch('http://localhost:5000/toshofeedbacks');
+    const jsonData = await toshoData.json();
+
+    const allToshoData = Object.values(jsonData);
+
+    return {
+        props : {
+            allToshoData,
+        },
+    }
+
+};
+
+export default function ToshoPosts({allToshoData}) {
+
   return (
     <>
       <Head>
@@ -13,7 +28,22 @@ export default function ToshoPosts({}) {
 
       <main className={styles.main}>
         <div className={styles.description}>
-
+            {allToshoData.map(({text, rating,peopleName,userName}) => (
+                <div className={styles.card}>
+                <div className={''}>
+                   {rating}
+                </div>
+                    <div>
+                      {text}
+                    </div>
+                 <div>
+                   {peopleName}
+                 </div>
+                  <div>
+                   {userName}
+                  </div>
+                 </div>
+            ))}
         </div>
         <Layout>
           <h2>
