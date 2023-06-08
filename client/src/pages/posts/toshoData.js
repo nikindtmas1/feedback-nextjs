c285333a35af7f439672e57f682a398420075809
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-06-08 12:16:01 +0300

    Update function ToshoPosts

--- client/src/pages/posts/toshoData.js
+++ client/src/pages/posts/toshoData.js
@@ -1,27 +1,26 @@
 import Head from "next/head";
 import Layout from "../../../components/layout";
-import styles from '../../styles/Home.module.css';
+import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
-export default async function ToshoPosts({}) {
-    return (
-        <>
-            <Head>
-                <title>
-                    Tosho Data
-                </title>
-            </Head>
-            <h2>
-                This Tosho Data Page
-            </h2>
+export default function ToshoPosts({}) {
+  return (
+    <>
+      <Head>
+        <title>Tosho Data</title>
+      </Head>
+      <h2>This Tosho Data Page</h2>
 
-            <main className={styles.main}>
-                <Layout>
-                <h2>
-                    <Link href='/'>← Back to home</Link>
-                </h2>
-                </Layout>
-            </main>
-        </>
-    )
-};
+      <main className={styles.main}>
+        <div className={styles.description}>
+
+        </div>
+        <Layout>
+          <h2>
+            <Link href="/">← Back to home</Link>
+          </h2>
+        </Layout>
+      </main>
+    </>
+  );
+}
