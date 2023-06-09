059ffd6ab29609d0b99947c26ff4a9ecdc6b09fc
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-06-09 13:40:10 +0300

    Create function TomiPosts

---
+++ client/src/pages/posts/tomiData.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function TomiPosts(){
+
+    return(
+        <>
+            <Head>
+                <title>
+                  Tomi Page
+                </title>
+            </Head>
+        </>
+    )
+}
