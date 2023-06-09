03fb5fa0e6f89391f595b1176cc8cebd44d3f548
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-06-09 13:51:00 +0300

    Create function getServerSideProps

--- client/src/pages/posts/tomiData.js
+++ client/src/pages/posts/tomiData.js
@@ -2,13 +2,27 @@ import Head from "next/head";
 import Layout from "../../../components/layout";
 import Link from "next/link";
 
-export default function TomiPosts() {
+export async function getServerSideProps() {
+    const tomiData = await fetch('http://localhost:5000/tomifeedbacks');
+    const jsonData = await tomiData.json();
+
+    const allTomiData = Object.values(jsonData);
+
+    return {
+        props: {
+            allTomiData,
+        },
+    }
+};
+
+export default function TomiPosts({allTomiData}) {
+
   return (
     <>
       <Head>
         <title>Tomi Page</title>
       </Head>
-      <h2>This is Tomi Page</h2>
+      <h2>This is Tomi Data Page</h2>
       <main>
         <div></div>
         <Layout>
