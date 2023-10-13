ee82c3dfc91819e604fb58c75a9db211727663d0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-13 11:01:37 +0300

    Added input for userName

--- client/src/pages/posts/toshoData.js
+++ client/src/pages/posts/toshoData.js
@@ -30,6 +30,7 @@ export default function ToshoPosts({allToshoData}) {
           <label>Added Feedback</label>
           <input name="text" placeholder="Feedback"></input>
           <input name="rating" placeholder="Rating"></input>
+          <input name="userName" placeholder="Your Name"></input>
           <button type="submit">Submit</button>
         </form>
       </div>
