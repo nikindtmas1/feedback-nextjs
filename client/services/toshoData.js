09dacb9dd101a462c269a1d7b8be69e70dddb26b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-13 13:35:47 +0300

    Create toshoData.js

---
+++ client/services/toshoData.js
@@ -0,0 +1,10 @@
+
+import service from '../services/feedbackServices';
+
+const host = 'http://localhost:5000';
+
+service.settings.host = host;
+
+export async function createToshoFeedback(data) {
+    return await service.post(host + '/toshofeedbacks', data);
+ };
