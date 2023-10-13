2f6bb5497701cfcf79a3779b7cf5e2456440e9a9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-13 11:50:39 +0300

    Create component

---
+++ client/components/addFeedback.js
@@ -0,0 +1,17 @@
+
+export function AddFeedback(params) {
+  return (
+    <div>
+      <form>
+        <label>Added Feedback</label>
+        <input name="text" placeholder="Feedback"></input>
+        <input name="rating" placeholder="Rating"></input>
+        <input name="userName" placeholder="Your Name"></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+} 
+
+    
+
