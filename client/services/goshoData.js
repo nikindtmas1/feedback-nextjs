ef7457108bfcb014de8c2234880268457b3fe904
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-16 10:37:23 +0300

    Added function createGoshoFeedback

--- client/services/goshoData.js
+++ client/services/goshoData.js
@@ -3,3 +3,7 @@ import * as service from '../services/feedbackServices';
 const host = 'http://localhost:5000';
 
 service.settings.host = host;
+
+export async function createGoshoFeedback(data) {
+    return await service.post(host + "/goshofeedbacks", data);
+};
