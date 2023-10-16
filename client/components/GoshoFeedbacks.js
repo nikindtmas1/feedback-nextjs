607390fa3135fd3f24a1f3d90d2c86fc2371dd15
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-16 10:27:24 +0300

    Create states

--- client/components/GoshoFeedbacks.js
+++ client/components/GoshoFeedbacks.js
@@ -2,6 +2,10 @@ import {React, useState} from "react";
 
 
 export function GoshoFeedbacks() {
+    const [text, setText] = useState("");
+    const [userName, setUserName] = useState("");
+    const [peopleName, setPeopleName] = useState("Gosho");
+    const [rating, setRating] = useState(10);
     
     return(
         <div>
