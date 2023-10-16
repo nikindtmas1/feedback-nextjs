8d509a950bde0b80b0cd9575183730ebcbd81ce3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-16 10:20:04 +0300

    Formated page

--- client/src/pages/posts/goshoData.js
+++ client/src/pages/posts/goshoData.js
@@ -1,77 +1,57 @@
 import Head from "next/head";
 import Layout from "../../../components/layout";
 import Link from "next/link";
-import styles from '../../styles/Home.module.css';
+import styles from "../../styles/Home.module.css";
 
 export async function getServerSideProps() {
-  const goshoData = await fetch('http://localhost:5000/goshofeedbacks');
+  const goshoData = await fetch("http://localhost:5000/goshofeedbacks");
   const jsonData = await goshoData.json();
 
   const allGoshoData = Object.values(jsonData);
 
   return {
     props: {
-        allGoshoData,
+      allGoshoData,
     },
-  }
-};
+  };
+}
 
-export default function GoshoPosts({allGoshoData}) {
-    return (
-        <>
-            <Head>
-                <title>
-                    Gosho Data
-                </title>
-            </Head>
-            <div>
-            <h2>
-            This is Gosho Data Page
-          </h2>
-            </div>
-          <div>
-            <form method="post">
-              <label>Added feedback: </label>
-              <input name="text" placeholder="Feedback"></input>
-              <input name="rating" placeholder="Rating"></input>
-              <input name="userName" placeholder="Your Name"></input>
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-            <main className={styles.main}>
+export default function GoshoPosts({ allGoshoData }) {
+  return (
+    <>
+      <Head>
+        <title>Gosho Data</title>
+      </Head>
+      <div className={styles.newfeedback}>
+        <h2>This is Gosho Data Page</h2>
+      </div>
+      <div>
+        <form method="post">
+          <label>Added feedback: </label>
+          <input name="text" placeholder="Feedback"></input>
+          <input name="rating" placeholder="Rating"></input>
+          <input name="userName" placeholder="Your Name"></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <main className={styles.main}>
         <div className={styles.description}>
-          {allGoshoData.map(({text, rating,peopleName,userName}) => (
-           <div className={styles.card}>
-          <div className={''}>
-             {rating}
-          </div>
-              <div>
-                {text}
-              </div>
-           <div>
-             {peopleName}
-           </div>
-            <div>
-             {userName}
+          {allGoshoData.map(({ text, rating, peopleName, userName }) => (
+            <div className={styles.card}>
+              <div className={""}>{rating}</div>
+              <div>{text}</div>
+              <div>{peopleName}</div>
+              <div>{userName}</div>
             </div>
-           </div>
-           
           ))}
-       
         </div>
 
-      <Layout>
-                <h2>
-                    <Link href='/'>← Back to home</Link>
-                </h2>
-      </Layout>
+        <Layout>
+          <h2>
+            <Link href="/">← Back to home</Link>
+          </h2>
+        </Layout>
       </main>
-        {/* <Layout>
-            <h2>
-            <Link href='/'>← Back to home</Link>
-            </h2>
-        </Layout> */}
-
-        </>
-    );
+    </>
+  );
 }
