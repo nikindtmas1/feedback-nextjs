682a842cc3e7238e58fcf1b1f7085a838d34d103
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-10-18 12:57:20 +0300

    Create function TomiFeedback

--- client/components/TomiFeedback.js
+++ client/components/TomiFeedback.js
@@ -1,3 +1,12 @@
 import React, {useState} from "react";
 import { fetchTomiDataToAPI } from "../utils/api";
 import styles from "../src/styles/Home.module.css";
+
+export function TomiFeedback() {
+    
+    return(
+        <div>
+
+        </div>
+    );
+};
